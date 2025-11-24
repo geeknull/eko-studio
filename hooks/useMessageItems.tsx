5c14682fdfa51fb26b9349f97bd524b9dd8a0415
geeknull <geeknull@gmail.com>
2025-11-25 01:37:22 +0800

    refactor: extract component for directory name casing

--- hooks/useMessageItems.tsx
+++ hooks/useMessageItems.tsx
@@ -5,7 +5,7 @@ import { Avatar, Tag, Button, Space } from 'antd'
 import { UserOutlined, RobotOutlined, EyeOutlined } from '@ant-design/icons'
 import { ChatMessage } from '@/store/chatStore'
 import { StreamCallbackMessage } from '@/types'
-import { MessageRenderer } from '@/components/message-renderer'
+import { MessageRenderer } from '@/components/messageRenderer'
 
 interface UseMessageItemsOptions {
   onViewJson?: (message: ChatMessage) => void
